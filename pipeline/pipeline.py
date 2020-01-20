0f36d04e71c1ad8e272408af445b90f1d88c1e76
Sharon Kuninin <schnitzelftw@gmail.com>
2020-01-20 18:07:11 +0200

    Log output to a file

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,9 +1,21 @@
 import datetime
+import os
+import time
 from typing import Any
+from typing import List
+from typing import NamedTuple
 
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+
+
+class FoldSummary(NamedTuple):
+    train_start: datetime.datetime
+    train_end: datetime.datetime
+    test_start: datetime.datetime
+    test_end: datetime.datetime
+    score: float
 
 
 class Pipeline:
@@ -37,9 +49,13 @@ class Pipeline:
         date_4 = datetime.datetime(year=2016, month=10, day=1)
         date_5 = datetime.datetime(year=2017, month=1, day=1)
 
-        for train_start, train_end, test_start, test_end in [(date_1, date_2, date_2, date_3),
-                                                             (date_1, date_3, date_3, date_4),
-                                                             (date_1, date_4, date_4, date_5)]:
+        summaries: List[FoldSummary] = []
+
+        for train_start, train_end, test_start, test_end in [
+            (date_1, date_2, date_2, date_3),
+            (date_1, date_3, date_3, date_4),
+            # (date_1, date_4, date_4, date_5)
+        ]:
             print('Calculating train and test datasets')
             train_df = df[(df['timestamp'] >= train_start) & (df['timestamp'] < train_end)]
             test_df = df[(df['timestamp'] >= test_start) & (df['timestamp'] < test_end)]
@@ -61,12 +77,32 @@ class Pipeline:
             score = self._calculate_score(predictions, test_df['meter_reading'])
             print(f'Score: {score}')
 
+            summaries.append(FoldSummary(
+                train_start=train_start,
+                train_end=train_end,
+                test_start=test_start,
+                test_end=test_end,
+                score=score
+            ))
 
-# TODO: switch to correct score function
-# TODO: output logs to log files
+        filename = f'../resources/runs/{time.time()}.txt'
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'w+') as f:
+            f.write(f'{model.__class__.__name__}\n')
+            f.write(f'{str(model.get_params())}\n')
+            for summary in summaries:
+                f.write(f'Summary (\n'
+                        f'\ttrain start: {summary.train_start}\n'
+                        f'\ttrain end: {summary.train_end}\n'
+                        f'\ttest start: {summary.test_start}\n'
+                        f'\ttest end: {summary.test_end}\n'
+                        f'\tscore: {summary.score}\n'
+                        f')\n')
+
+        print(summaries)
 
 
 if __name__ == '__main__':
     pipeline = Pipeline()
-    pipeline.run(df=pd.read_csv('../resources/train.csv'),
+    pipeline.run(df=pd.read_csv('../resources/train.csv', nrows=10000000),
                  model=RandomForestRegressor(n_estimators=4))
